629ff70f823bfca80e06841473080e1e74bbf4a8
Ivan Artamonov <ivan.s.artamonov@gmail.com>
2019-06-16 13:06:33 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Evert::E - It's new $module
+
+# SYNOPSIS
+
+    use Evert::E;
+
+# DESCRIPTION
+
+Evert::E is ...
+
+# LICENSE
+
+Copyright (C) Ivan Artamonov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ivan Artamonov <ivan.s.artamonov@gmail.com>
---
+++ lib/Evert/E.pm
@@ -0,0 +1,39 @@
+package Evert::E;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Evert::E - It's new $module
+
+=head1 SYNOPSIS
+
+    use Evert::E;
+
+=head1 DESCRIPTION
+
+Evert::E is ...
+
+=head1 LICENSE
+
+Copyright (C) Ivan Artamonov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ivan Artamonov E<lt>ivan.s.artamonov@gmail.comE<gt>
+
+=cut
+
